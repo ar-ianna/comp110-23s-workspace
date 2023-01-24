ed4f6c6eaed129d27015e026f70fa75f043ebf76
Arianna Russell <your@primary.emailarussell@unc.edu>
2023-01-24 17:47:35 -0500

    Progress on Exercise 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -7,10 +7,10 @@ if len(chardle_guess) == 5:
     if len(character_search) == 1:
         print("Searching for " + character_search + " in " + chardle_guess) 
 else:
-    len(chardle_guess)!= 5
+    len(chardle_guess) != 5
     print("Error: Word must contain 5 characters")
     exit()
-if len(character_search)!= 1:
+if len(character_search) != 1:
     print("Error: Character must be a single character.")
     exit()
 frequency: int = 0
@@ -30,8 +30,10 @@ if chardle_guess[len(chardle_guess) - 4] == character_search:
 if chardle_guess[len(chardle_guess) - 5] == character_search:
     print(character_search + " found at index 0")
     frequency = frequency + 1
-if frequency > 0:
-    print (str(frequency) + " instances of " + character_search + " found in " + chardle_guess)
+if frequency == 0:
+    print("No instances of " + character_search + " found in " + chardle_guess)
 else: 
-    if frequency == 0:
-        print ("No instance of " + character_search + " found in " + chardle_guess)
+    if frequency == 1:
+        print("1 instance of " + character_search + " found in " + chardle_guess)
+if frequency > 1:
+    print(str(frequency) + " instances of " + character_search + " found in " + chardle_guess)
