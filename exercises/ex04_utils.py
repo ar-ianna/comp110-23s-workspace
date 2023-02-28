2ee10fcf981392878030ef361b0c734c76852e84
Arianna Russell <your@primary.emailarussell@unc.edu>
2023-02-28 16:51:12 -0500

    Progress on EX04

---
+++ exercises/ex04_utils.py
@@ -0,0 +1,36 @@
+"""EX04 - 'list' Utility Functions"""
+___author___ = "730488361"
+
+def all(a: list[int], b: int) -> bool:
+    """Test if given int is the same as all in list"""
+    i: int = 0
+    while i < len(a):
+        if b != a[i]:
+            return False
+        i += 1
+    return True
+
+def max(input: list[int]) -> int:
+    """Return largest int in list"""
+    i: int = 0
+    largest: int
+    if len(input) == 0:
+        raise ValueError("max() arg is an empty List")
+    while i < len(input):
+        largest = input[i]
+        i += 1
+        if largest < input[i]:
+            largest = input[i]
+            return largest
+        elif largest > input[i]:
+            i += 1
+            return largest
+        
+def is_equal(x: list[int], y: list[int]) -> bool:
+    """Test if lists are equal to each other"""
+    i: int = 0
+    while i < len(x) and i<len(y):
+        if x[i] != y[i]:
+            return False
+        i += 1
+    return True
